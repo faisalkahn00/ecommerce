fc1a60c38ace5ed55b004d3b00c925984a8a1d57
faisalkahn00 <114398774+faisalkahn00@users.noreply.github.com>
2022-09-26 04:55:14 -0700

    Add files via upload

---
+++ final project/index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<!-- Created By CodingNepal - www.Technologywang.com -->
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Responsive Navbar | CodingWang</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="final.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+  </head>
+  <body>
+    <nav>
+      <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label >
+      <label class="logo" > ARZANO  </label>
+      <!-- <label for="">
+         <img class="logo_img" src="./logo-removebg-preview.png" height="70px" alt="">
+      </label> -->
+      <ul>
+        <li><a class="active" href="#">Store</a></li>
+        <li><a href="#">Today's Deal ARZANO</a></li>
+        <li><a href="./login.php">Sell</a></li>
+        <li><a href="#">Customer Service</a></li>
+        <li><a href="#">Log In</a></li>
+      </ul>
+    </nav>
+    
+    <h1>ARZANO PRODUCTS LISTS</h1>
+    <br><br>
+
+    <div class="products">
+
+    <div class="cart">
+      <div class="img">
+        <img src="./1.jpg" alt="" width="200px" height="200px">
+      </div>
+
+      <div class="content">
+        <h3>feshion week</h3>
+        <p>the suit is disgin for girls</p>
+       <b> <p class="price">RS: 500</p> </b>
+        <br>
+        <a href="./product_info.html"><button class="btn">add to card</button></a>
+      </div>
+    </div>
+
+
+    
+      
+    <div class="cart">
+      <div class="img">
+        <img src="./3.jpg" alt=""  width="200px" height="200px">
+      </div>
+
+      <div class="content">
+        <h3>office suit</h3>  
+        <p>the suit is disgin for office</p>
+        <b> <p class="price">RS: 1100</p></b>
+
+        <br>
+        <a href="./product_info.html"><button class="btn">add to card</button></a>
+      </div>
+      </div>
+
+
+
+      
+    <div class="cart">
+      <div class="img">
+        <img src="./6.jpg" alt="" width="200px" height="200px">
+      </div>
+
+      <div class="content">
+        <h3>photo shoot dress</h3>  
+        <p>the suit is disgin for photo shoot</p>
+        <b> <p class="price">RS: 699</p> </b>
+
+        <br>
+        <a href="./product_info.html"><button class="btn">add to card</button></a>
+      </div>
+      </div>
+
+</div>
+
+      <br><br>
+
+      <div class="products2">
+    <div class="cart">
+      <div class="img">
+        <img src="./4.jpg" alt="" width="200px" height="200px">
+      </div>
+
+      <div class="content">
+        <h3>desgin for men</h3>  
+        <p>the suit is disgin for men's</p>
+        <b> <p class="price">RS: 899</p> </b>
+
+        <br>
+         <a href="./product_info.html"><button class="btn">add to card</button></a>
+      </div>
+      </div>
+
+
+
+      
+    <div class="cart">
+      <div class="img">
+        <img src="./5.jpg" alt="" width="200px" height="200px">
+      </div>
+
+      <div class="content">
+        <h3>girls winter collection</h3>  
+        <p>the suit is desgin for winter</p>
+        <b> <p class="price">RS: 999</p> </b>
+
+         <br>
+        <a href="./product_info.html"><button class="btn">add to card</button></a>
+      </div>
+      </div>
+
+
+      
+    <div class="cart">
+      <div class="img">
+        <img src="./7.jpg" alt="" width="200px" height="200px">
+      </div>
+
+      <div class="content">
+        <h3>white dress</h3>  
+        <p>the suit is disgin for white collection</p>
+        <b> <p class="price">RS: 1299</p> </b>
+
+        <br>
+        <a href="./product_info.html"><button class="btn">add to card</button></a>
+      </div>
+      </div>
+
+
+    </div>
+
+  
+  </body>
+</html>
